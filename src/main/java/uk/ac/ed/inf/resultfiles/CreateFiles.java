5d12c81ea4e50c993408282597283d4426d6ce4f
MattDavies-code <maffdavies9@gmail.com>
2023-11-30 12:38:03 +0000

    Final

--- src/main/java/uk/ac/ed/inf/resultfiles/CreateFiles.java
+++ src/main/java/uk/ac/ed/inf/resultfiles/CreateFiles.java
@@ -142,12 +142,6 @@ public class CreateFiles {
             Gson gson = createGsonWithLocalDateSerializer();
             String fileName = "drone-" + date + ".geojson";
             FileWriter writer = new FileWriter(resultfiles.resolve(fileName).toString(), false);
-            // if there are no deliveries, create an empty file with just {} in it
-            if (flightPaths.isEmpty()) {
-                writer.write("{}");
-                writer.close();
-                return;
-            }
             gson.toJson(createGeoJsonFeatureCollection(flightPaths), writer);
             writer.close();
         } catch (IOException e) {
